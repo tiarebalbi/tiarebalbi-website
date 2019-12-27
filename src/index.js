872ea4171df51de76606cb6d4c7625529e2e7613
Tiarê Balbi <tiarebalbi@users.noreply.github.com>
2019-12-27 16:32:12 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import '@atlaskit/css-reset/dist/bundle.css';
 
 import App from './containers/App';
 
 const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+render(<App />, rootElement);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
