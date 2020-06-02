0d4eeb5aa6d6fda438b32e9e9c3b296fa9964eda
Tiarê Balbi Bonamini <me@tiarebalbi.com>
2020-06-02 06:23:52 +0100

    Bump version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import * as React from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import '@atlaskit/css-reset/dist/bundle.css';
 import 'highlight.js/styles/atom-one-dark.css';
-import App from './containers/App';
+import loadable from '@loadable/component';
+import LoadingView from './components/Common/LoadingView';
+
+const LoadableApp = loadable(() => import('./containers/App'), {
+  fallback: LoadingView,
+});
 
 const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+render(<LoadableApp />, rootElement);
 
 serviceWorker.register();
