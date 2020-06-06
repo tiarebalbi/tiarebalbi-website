0edad843f5658813d4a52705d8c91de2cf0f2bdb
Tiarê Balbi <tiarebalbi@users.noreply.github.com>
2020-06-06 12:48:15 +0100

    Adding snapshot (#269)
    * Adding snapshot
    
    * Adding pupper options

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
+
 import * as serviceWorker from './serviceWorker';
 import '@atlaskit/css-reset/dist/bundle.css';
 import 'highlight.js/styles/atom-one-dark.css';
 import App from './containers/App';
 
 const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 serviceWorker.register();
