6d4c2b126f07ecb27d63ebf46820613b4e59ca90
Tiarê Balbi <tiarebalbi@users.noreply.github.com>
2021-02-24 08:34:09 +0000

    Microsoft Clarity

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -17,6 +17,17 @@ const GoogleAnalytics = () => (
                 gtag("config", "G-8G0QDFHYFC");`,
         }}
       />
+  
+      <script
+        async
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "5k25bhyyoe");`,
+        }}
+      />
     </Head>
   </>
 );
