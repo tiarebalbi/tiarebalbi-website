861551726b9ca48c0c4cd22f05e4943d50c3c260
Tiare Balbi <me@tiarebalbi.com>
2021-03-26 04:34:08 +0000

    Fixes #310

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,8 @@
 module.exports = {
   siteUrl: 'https://tiarebalbi.com',
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'], // <= exclude here
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://tiarebalbi.com/server-sitemap.xml']
+  }
 };
---
+++ pages/server-sitemap.xml/index.js
@@ -0,0 +1,27 @@
+import { getServerSideSitemap } from 'next-sitemap';
+import { client } from '../../lib/graphql';
+import { allBlogPostsQuery } from '../../lib/queries';
+
+export const getServerSideProps = async (ctx) => {
+  const domain = 'https://tiarebalbi.com';
+  const response = await client.query(allBlogPostsQuery);
+
+  const posts = response?.data?.allBlog_posts?.edges;
+  console.log(JSON.stringify(posts));
+
+  const fields = [];
+
+  posts.map((record) => {
+    const date = record?.node?.created_date || '2021-03-26';
+    fields.push({
+      loc: `${domain}/articles/${record?.node?._meta?.uid}`,
+      lastmod: new Date(date).toISOString(),
+      priority: 0.8
+    });
+  });
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+// Default export to prevent next.js errors
+export default function PageSiteMap() {}
