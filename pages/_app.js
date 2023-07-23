39309dac1bbcfd6fbf8471340b108dc7639513f7
Tiarê Balbi <me@tiarebalbi.com>
2023-07-23 10:16:59 -0700

    Chore: pixel api

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/flexboxgrid.css';
 
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import { useRouter } from 'next/router';
 import { Analytics } from '@vercel/analytics/react';
 
@@ -30,8 +31,25 @@ function AppLoader({ Component, pageProps }) {
         <link rel="shortlink" href={refLink} />
         <link rel="preconnect" href="https://tiarebalbi.com" />
         <link rel="dns-prefetch" href="https://tiarebalbi.com" />
+        <Script>
+          {`
+            !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1545960279561693');
+              fbq('track', 'PageView');  
+          `}
+        </Script>
       </Head>
       <main className={styles.containerHolder}>
+        <noscript>
+          <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1545960279561693&ev=PageView&noscript=1" />
+        </noscript>
         <div className={styles.content}>
           <PageHeader />
           <Analytics />
