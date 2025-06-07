33f8cabce85ac81d3df44eb37244c78882a94f45
Tiarê Balbi <tiarebalbi@users.noreply.github.com>
2025-06-07 15:38:19 -0700

    Rework 404 page and revert yarn state

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
-const Custom404 = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/10', undefined, { shallow: true })
-  }, [router])
-
-  return <div />;
-};
+const Custom404 = () => (
+  <>
+    <Head>
+      <title>Page Not Found</title>
+    </Head>
+    <main className="container mx-auto p-8 text-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4">The page you are looking for does not exist.</p>
+      <Link className="text-blue-500 underline" href="/">Go back home</Link>
+    </main>
+  </>
+);
 
 export default Custom404;
